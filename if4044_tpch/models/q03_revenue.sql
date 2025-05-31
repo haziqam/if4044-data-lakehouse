cb3a61d08e8bd5e107facd5e272aa0160f21c357
rayhanmp <rayhanmpramanda@gmail.com>
2025-05-31 16:31:10 +0700

    feat(dbt): revise q03 query to align with tpc-h spec

--- if4044_tpch/models/q03_revenue.sql
+++ if4044_tpch/models/q03_revenue.sql
@@ -1,22 +1,22 @@
--- models/tpch/q03_revenue.sql
+-- models/if4044-_tpch/q03_revenue.sql
 
 SELECT
-    c.c_custkey,
-    c.c_name,
+    l.l_orderkey,
     SUM(l.l_extendedprice * (1 - l.l_discount)) AS revenue,
     o.o_orderdate,
     o.o_shippriority
-FROM {{ source('tpch', 'customer') }} AS c
-JOIN {{ source('tpch', 'orders') }} AS o
-    ON c.c_custkey = o.o_custkey
-JOIN {{ source('tpch', 'lineitem') }} AS l
-    ON l.l_orderkey = o.o_orderkey
+FROM
+    {{ source('tpch', 'customer') }} AS c,
+    {{ source('tpch', 'orders') }} AS o,
+    {{ source('tpch', 'lineitem') }} AS l
 WHERE
-    o.o_orderdate < DATE '1995-03-15'
-    AND l.l_shipdate > DATE '1995-03-15'
+    c.c_mktsegment = 'BUILDING' -- RANDOMLY SELECTED WITHIN THE LIST OF VALUES DEFINED FOR SEGMENTS IN CLAUSE 4.2.2.13
+    AND c.c_custkey = o.o_custkey
+    AND l.l_orderkey = o.o_orderkey
+    AND o.o_orderdate < DATE '1995-03-15' --  RANDOMLY SELECTED DAY WITHIN [1995-03-01 .. 1995-03-31]
+    AND l.l_shipdate > DATE '1995-03-15' -- RANDOMLY SELECTED DAY WITHIN [1995-03-01 .. 1995-03-31]
 GROUP BY
-    c.c_custkey,
-    c.c_name,
+    l.l_orderkey,
     o.o_orderdate,
     o.o_shippriority
 ORDER BY
